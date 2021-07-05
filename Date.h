25faa25494c076011ba277774095019ee8820ffc
aedelgado19 <aedelgado19@gmail.com>
2021-07-05 12:45:39 -0700

    making classes

---
+++ Date.h
@@ -0,0 +1,6 @@
+/* Author: Allison Delgado
+   Class: CS202 Summer 2021
+   [desc]
+   
+   Last updated: July 5, 2021
+ */
---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* Author: Allison Delgado
+   Class: CS202 Summer 2021
+   The main class acts as a testing site for
+   all public member functions in the classes.
+   The aim of this program is to act as a scheduling
+   tool for various tasks.
+
+   Last updated: July 5, 2021
+ */
+
