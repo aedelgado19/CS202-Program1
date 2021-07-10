b6041be412511d14dd2de4ffd8b10a672be9ad1c
aedelgado19 <aedelgado19@gmail.com>
2021-07-09 17:10:02 -0700

    lol

--- main.cpp
+++ main.cpp
@@ -9,8 +9,30 @@
  */
 
 #include <iostream>
+#include "Event.h"
 using namespace std;
 
 int main(){
+  cout << "Welcome to Event Planner!" << endl;
 
+  
+  Beach b;
+  Restaurant r;
+  Shopping s;
+  
+  
+  char choice = 'a';
+  
+  do {
+    cout << "Select an event to plan." << endl;
+    cout << "1 - Beach Trip" << endl;
+    cout << "2 - Shopping Trip" << endl;
+    cout << "3 - Restaurant" << endl;
+    cout << "q - quit program" << endl;
+    cin >> choice;
+    cin.ignore(1000, '\n');
+  } while(choice != 'q'); 
+  
+  
+  return 0;
 }
